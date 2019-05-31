5bff39a11f6ee6dd2c355b8b2c6fc1ce314b5292
melxi <magomed.futijev@gmail.com>
2019-05-31 23:59:23 +0300

    Image results component displaying images

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ImageResults from "../image-results/ImageResults";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
@@ -14,22 +15,34 @@ class Search extends Component {
   };
 
   onTextChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiURL}/?key=${this.state.apiKey}&q=${
-            this.state.searchText
-          }&image-type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then(res =>
-          this.setState({
-            images: res.data.hits
-          })
-        )
-        .catch(err => console.log(err));
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === "") {
+        this.setState({
+          images: []
+        });
+      } else {
+        axios
+          .get(
+            `${this.state.apiURL}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image-type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res =>
+            this.setState({
+              images: res.data.hits
+            })
+          )
+          .catch(err => console.log(err));
+      }
     });
   };
 
+  onAmountChange = (e, index, value) => {
+    this.setState({
+      amount: value
+    });
+  };
   render() {
     console.log(this.state.images);
     return (
@@ -54,6 +67,10 @@ class Search extends Component {
           <MenuItem value={30} primaryText="30" />
           <MenuItem value={50} primaryText="50" />
         </SelectField>
+        <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
